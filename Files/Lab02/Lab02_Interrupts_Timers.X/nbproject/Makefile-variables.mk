9f00c19a57094ca9454180253462167de8ae79d1
Iliasu Salaudeen <salaudeenilyas@gmail.com>
2025-06-02 23:53:21 +0200

    lab02

---
+++ Files/Lab02/Lab02_Interrupts_Timers.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Lab02_Interrupts_Timers.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Lab02_Interrupts_Timers.X.production.hex
