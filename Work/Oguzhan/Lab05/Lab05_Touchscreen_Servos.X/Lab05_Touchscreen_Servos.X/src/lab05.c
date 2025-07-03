81c91b129de6e472e30251e22090895ea51f43e2
Oğuzhan Eşen <97829211+esenoguzhan@users.noreply.github.com>
2025-07-03 15:58:31 +0200

    Update lab05.c

--- Work/Oguzhan/Lab05/Lab05_Touchscreen_Servos.X/Lab05_Touchscreen_Servos.X/src/lab05.c
+++ Work/Oguzhan/Lab05/Lab05_Touchscreen_Servos.X/Lab05_Touchscreen_Servos.X/src/lab05.c
@@ -33,13 +33,40 @@ void init_servo(char axis) {
 
         SETBIT(T2CONbits.TON); // start Timer2
     }
-    // Add Y-axis (e.g., OC7/Timer3) if available
+
+    else if (axis == 'Y') {
+        CLEARBIT(TRISDbits.TRISD6); // OC7 as output
+
+        // Timer3 setup
+        CLEARBIT(T3CONbits.TON);
+        CLEARBIT(T3CONbits.TCS);
+        CLEARBIT(T3CONbits.TGATE);
+        TMR3 = 0x00;
+        T3CONbits.TCKPS = TCKPS_64;
+        PR3 = PR2_VALUE;  // same 20ms PWM period
+        CLEARBIT(IFS0bits.T3IF);
+        CLEARBIT(IEC0bits.T3IE);
+
+        // OC7 setup
+        OC7CON = 0x0000;
+        OC7R = 1500 * (FCY / PRESCALER) / 1000000;
+        OC7RS = OC7R;
+        OC7CON = 0x0006; // PWM mode, use Timer3
+
+        SETBIT(T3CONbits.TON); // start Timer3
+    }
+    set_servo('X',1500);
+    set_servo('Y',1500);
+    __delay_ms(1000);
 }
 
+
 void set_servo(char axis, uint16_t duty_us) {
-    uint16_t compare_val = (duty_us * (FCY / PRESCALER)) / 1000000;
+    uint16_t compare_val = (-1*(duty_us-20000) * (FCY / PRESCALER)) / 1000000;
     if (axis == 'X') {
         OC8RS = compare_val;
+    } else if (axis == 'Y') {
+        OC7RS = compare_val;  // Y-axis servo on OC7
     }
 }
 
@@ -50,8 +77,10 @@ void init_touchscreen() {
     SETBIT(TRISEbits.TRISE2);
     SETBIT(TRISEbits.TRISE3);
 
-    CLEARBIT(AD1PCFGHbits.PCFG15); // AN15
-    CLEARBIT(AD1PCFGHbits.PCFG9);  // AN9
+
+    AD1PCFGHbits.PCFG20 = 1; // RE8 as digital (for buttons)
+    AD1PCFGLbits.PCFG15 = 0; // AN15 as analog input (X)
+    AD1PCFGLbits.PCFG9  = 0; // AN9 as analog input (Y)
 
     AD1CON1bits.AD12B = 0;
     AD1CON1bits.FORM = 0;
@@ -64,6 +93,7 @@ void init_touchscreen() {
 
     SETBIT(AD1CON1bits.ADON);
 }
+
 
 void set_touchscreen_axis(char axis) {
     if (axis == 'X') {
@@ -84,6 +114,7 @@ void set_touchscreen_axis(char axis) {
     __delay_ms(10);
 }
 
+
 uint16_t read_touchscreen() {
     if (!AD1CON1bits.ADON) return 0;
 
@@ -99,35 +130,63 @@ uint16_t read_touchscreen() {
     return ADC1BUF0;
 }
 
-typedef enum {CORNER_1, CORNER_2, CORNER_3, CORNER_4} Corner;
 
-void move_ball_to_corner(Corner corner) {
+
+
+
+
+
+
+//typedef enum {CORNER_1, CORNER_2, CORNER_3, CORNER_4} Corner;
+
+void move_ball_to_corner(int corner) {
     switch (corner) {
-        case CORNER_1: set_servo('X', 2000); break;
-        case CORNER_2: set_servo('X', 1000); break;
-        case CORNER_3: set_servo('X', 1000); break;
-        case CORNER_4: set_servo('X', 2000); break;
+        case 3:
+            set_servo('X', 2000);
+            set_servo('Y', 2000);
+            break;
+        case 2:
+            set_servo('X', 1000);
+            set_servo('Y', 2000);
+            break;
+        case 1:
+            set_servo('X', 1000);
+            set_servo('Y', 1000);
+            break;
+        case 0:
+            set_servo('X', 2000);
+            set_servo('Y', 1000);
+            break;
     }
 }
+
 
 void print_position(uint16_t x, uint16_t y) {
     lcd_clear();
     lcd_locate(0, 0);
-    lcd_printf("X: %u, Y: %u\\r", x, y);
+    lcd_printf("X: %u, Y: %u", x, y);
+    
 }
+
+
 
 void main_loop() {
     lcd_printf("Lab05: Touchscreen &\\r\\nServos");
     lcd_locate(0, 2);
-    lcd_printf("Group: YourName");
+    lcd_printf("Group: 06");
 
+    
     init_servo('X');
+    init_servo('Y');
     init_touchscreen();
 
-    Corner corner = CORNER_1;
-
+    int corner = 0;
+    
+    
+    
     while (1) {
         move_ball_to_corner(corner);
+        //set_servo("X", 3820);
 
         __delay_ms(1000);
         set_touchscreen_axis('X');
@@ -137,8 +196,11 @@ void main_loop() {
         uint16_t y = read_touchscreen();
 
         print_position(x, y);
+       
 
         __delay_ms(4000);
         corner = (corner + 1) % 4;
+        lcd_locate(0, 4);
+        lcd_printf("corner: %d", corner);
     }
 }
